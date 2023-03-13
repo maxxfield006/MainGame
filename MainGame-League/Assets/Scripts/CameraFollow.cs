8bde65c11fd59356300489c1c1a7723206231ebb
Max Field <maxfield7766@gmail.com>
2023-03-13 22:14:22 +1300

    Work1

---
+++ MainGame-League/Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    public GameObject player;
+
+    //offset the camera to see player better
+    float offsetX = 0;
+    float offsetZ = -5.25f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //makes the camera only follow the x and z, so when moving it doesnt rotate the camera or change the height
+        transform.position = new Vector3(player.transform.position.x + offsetX, 8.5f, player.transform.position.z + offsetZ);
+    }
+}
---
+++ MainGame-League/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class PlayerController : MonoBehaviour
+{
+    NavMeshAgent champ;
+
+    void Start()
+    {
+        champ = GetComponent<NavMeshAgent>();
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(1))
+        {
+            RaycastHit hit;
+
+            if (Physics.Raycast(Camera.main.ScreenPointToRay(Input.mousePosition), out hit, 100)) 
+            {
+                champ.destination = hit.point;
+            }
+        }
+    }
+}
