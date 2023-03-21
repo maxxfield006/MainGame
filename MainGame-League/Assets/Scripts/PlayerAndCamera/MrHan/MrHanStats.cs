e35e4f0c647b72162a91abed398bca19c0dd99c6
Max Field <maxfield7766@gmail.com>
2023-03-21 23:02:43 +1300

    agf

---
+++ MainGame-League/Assets/Scripts/PlayerAndCamera/MrHan/MrHanStats.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MrHanStats : MonoBehaviour
+{
+    public float health = 560;
+    public float healthReg = 2;
+    public float healthScale = 110;
+    public float healthRegScale = 0.3f;
+
+    public float mana = 320;
+    public float manaReg = 2;
+    public float manaScale = 60;
+    public float manaRegScale = 1;
+
+    public float attackDmg = 60;
+    public float attackPower = 0;
+    public float attackDmgScale = 12;
+
+    public float attackSpeed = 0.65f;
+    public float attackSpeedScale = 0.05f;
+
+    public float critChance = 0;
+
+    public float armor = 28;
+    public float magicRes = 32;
+    public float armorScale = 6;
+    public float magicResScale = 7;
+
+
+    public float moveSpeed = 340;
+    public float moveSpeedScale = 5;
+
+
+    void Start()
+    {
+        
+    }
+
+    
+    void Update()
+    {
+        
+    }
+}
