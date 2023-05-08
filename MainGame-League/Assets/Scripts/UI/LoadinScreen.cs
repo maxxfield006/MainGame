c80dd6a60bfd4d22e90ec8da838238403e8c6ea9
Max Field <maxfield7766@gmail.com>
2023-05-08 13:11:45 +1200

    hkgiu

---
+++ MainGame-League/Assets/Scripts/UI/LoadinScreen.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class LoadinScreen : MonoBehaviour
+{
+    public Button startButton;
+    public RawImage img;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Button startBtn = startButton.GetComponent<Button>();
+        RawImage image = img.GetComponent<RawImage>();
+
+        startBtn.onClick.AddListener(lowerOpacity);
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void lowerOpacity()
+    {
+        image.color = new Color(image.color.r, image.color.g, image.color.b, 1f);
+    }
+}
