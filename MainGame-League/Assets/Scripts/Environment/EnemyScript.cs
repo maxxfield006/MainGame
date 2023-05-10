1a0a92d11811448e7efbd949f25960ed0f230404
Max Field <maxfield7766@gmail.com>
2023-05-11 01:33:55 +1200

    ghjg

--- MainGame-League/Assets/Scripts/Environment/EnemyScript.cs
+++ MainGame-League/Assets/Scripts/Environment/EnemyScript.cs
@@ -4,15 +4,49 @@ using UnityEngine;
 
 public class EnemyScript : MonoBehaviour
 {
-    // Start is called before the first frame update
+    private GameObject redTower;
+    private GameObject blueTower;
+
+    private GameObject blueMinion;
+    private GameObject redMinion;
+
+    private float stopDistance = 2f;
+
+    private float minionMoveSpeed = 1.1f;
+
     void Start()
     {
+        redTower = GameObject.FindGameObjectWithTag("redTower");
+        blueTower = GameObject.FindGameObjectWithTag("blueTower");  
         
     }
 
-    // Update is called once per frame
+  
     void Update()
     {
-        
+        blueMinion = GameObject.FindGameObjectWithTag("blueMinion");
+        redMinion = GameObject.FindGameObjectWithTag("redMinion");
+
+        if (blueMinion != null)
+        {
+            float currentDistanceBM = Vector3.Distance(blueMinion.transform.position, redTower.transform.position);
+
+            if (currentDistanceBM > stopDistance)
+            {
+                Vector3 blueDirection = redTower.transform.position - blueMinion.transform.position;
+                blueDirection.Normalize();
+                blueMinion.transform.position += blueDirection * minionMoveSpeed * Time.deltaTime;
+            }
+  
+        }
+
+        if (redMinion != null)
+        {
+
+            Vector3 redDirection = blueTower.transform.position - redMinion.transform.position;
+            redDirection.Normalize();
+            blueMinion.transform.position += redDirection * minionMoveSpeed * Time.deltaTime;
+
+        }
     }
 }
