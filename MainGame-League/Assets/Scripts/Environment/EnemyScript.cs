7c70da96fd96653f25e5b23929aec9e55ff89a23
Max Field <maxfield7766@gmail.com>
2023-05-11 13:37:55 +1200

    ghfgh

--- MainGame-League/Assets/Scripts/Environment/EnemyScript.cs
+++ MainGame-League/Assets/Scripts/Environment/EnemyScript.cs
@@ -4,51 +4,102 @@ using UnityEngine;
 
 public class EnemyScript : MonoBehaviour
 {
-    private GameObject redTower;
-    private GameObject blueTower;
+    private GameObject[] redTowers;
+    private GameObject[] blueTowers;
 
     private GameObject blueMinion;
     private GameObject redMinion;
 
+    private GameObject blueTarget;
+    private GameObject redTarget;
+
+    private GameObject waypoint;
+    bool atWayPoint = false;
+
     private float stopDistance = 2f;
 
-    private float minionMoveSpeed = 1.1f;
+    private float minionMoveSpeed = 2f;
 
     void Start()
     {
-        redTower = GameObject.FindGameObjectWithTag("redTower");
-        blueTower = GameObject.FindGameObjectWithTag("blueTower");  
-        
+        redTowers = GameObject.FindGameObjectsWithTag("redTower");
+        blueTowers = GameObject.FindGameObjectsWithTag("blueTower");
+
+        blueMinion = GameObject.FindGameObjectWithTag("blueMinion");
+        redMinion = GameObject.FindGameObjectWithTag("redMinion");
+
+        waypoint = GameObject.FindGameObjectWithTag("waypoint");
+
+        float currentDistanceToWaypointBlue = Vector3.Distance(blueMinion.transform.position, waypoint.transform.position);
+
+        if (currentDistanceToWaypointBlue > stopDistance)
+        {
+            Vector3 blueDirection = waypoint.transform.position - blueMinion.transform.position;
+            blueDirection.Normalize();
+            blueMinion.transform.position += blueDirection * minionMoveSpeed * Time.deltaTime;
+            atWayPoint = true;
+        }
+
+        float currentDistanceToWaypointRed = Vector3.Distance(redMinion.transform.position, waypoint.transform.position);
+
+        if (currentDistanceToWaypointRed > stopDistance)
+        {
+            Vector3 redDirection = waypoint.transform.position - redMinion.transform.position;
+            redDirection.Normalize();
+            redMinion.transform.position += redDirection * minionMoveSpeed * Time.deltaTime;
+        }
+
     }
 
   
     void Update()
     {
-        blueMinion = GameObject.FindGameObjectWithTag("blueMinion");
-        redMinion = GameObject.FindGameObjectWithTag("redMinion");
 
-        if (blueMinion != null)
+
+        if (blueMinion != null && atWayPoint)
         {
-            float currentDistanceBM = Vector3.Distance(blueMinion.transform.position, redTower.transform.position);
-
-            if (currentDistanceBM > stopDistance)
+            float minDistance2 = float.MaxValue;
+            foreach (GameObject redTower in redTowers)
             {
-                Vector3 blueDirection = redTower.transform.position - blueMinion.transform.position;
+                float distance = Vector3.Distance(blueMinion.transform.position, redTower.transform.position);
+                if (distance < minDistance2)
+                {
+                    minDistance2 = distance;
+                    redTarget = redTower;
+                }
+            }
+
+
+            if (atWayPoint)
+            {
+                Vector3 blueDirection = redTarget.transform.position - blueMinion.transform.position;
                 blueDirection.Normalize();
                 blueMinion.transform.position += blueDirection * minionMoveSpeed * Time.deltaTime;
             }
   
         }
 
-        if (redMinion != null)
-        {
-            float currentDistanceRM = Vector3.Distance(redMinion.transform.position, blueTower.transform.position);
+        
 
-            if (currentDistanceRM > stopDistance)
+        if (redMinion != null && atWayPoint)
+        {
+
+            float minDistance = float.MaxValue;
+            foreach (GameObject blueTower in blueTowers)
             {
-                Vector3 redDirection = blueTower.transform.position - redMinion.transform.position;
+                float distance = Vector3.Distance(blueMinion.transform.position, blueTower.transform.position);
+                if (distance < minDistance)
+                {
+                    minDistance = distance;
+                    blueTarget = blueTower;
+                }
+            }
+
+            if (atWayPoint)
+            {
+                Vector3 redDirection = blueTarget.transform.position - redMinion.transform.position;
                 redDirection.Normalize();
-                blueMinion.transform.position += redDirection * minionMoveSpeed * Time.deltaTime;
+                redMinion.transform.position += redDirection * minionMoveSpeed * Time.deltaTime;
             }
 
         }
