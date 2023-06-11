62d4c5b78139126d72686ba2a46996b41b003494
Max Field <maxfield7766@gmail.com>
2023-06-11 15:32:21 +1200

    fxcgbdxcfg

--- MainGame-League/Assets/Scripts/Networking/networkManager.cs
+++ MainGame-League/Assets/Scripts/Networking/networkManager.cs
@@ -1,44 +1,69 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using Photon.Pun;
 
-public class networkManager : Photon.MonoBehaviour
+public class networkManager : MonoBehaviourPunCallbacks
 {
     int state = 0;
 
     public int maxPlayers;
 
+    public GameObject mainGame;
+    public GameObject joinLobby;
+
+    float currentTime;
+    int minutes;
+    int seconds;
+
+    public GameObject mrHan;
+
+    [Space]
+    public Transform spawnPosition;
 
     void Start()
     {
         state = 0;
+        
     }
 
 
     void Update()
     {
+        if (state == 5)
+        {
+            currentTime += Time.deltaTime;
+            minutes = (int)(currentTime / 60);
+            seconds = (int)(currentTime % 60);
+        }
         
     }
 
     void OnGUI()
     {
+        GUIStyle styleBut = GUI.skin.GetStyle("Button");
+        styleBut.fontSize = 60;
+
+        GUIStyle styleLab = GUI.skin.GetStyle("Label");
+        styleLab.fontSize = 60;
+
         switch (state)
         {
 
             //start
             case 0:
-                if (GUI.Button(new Rect(10, 10, 200, 200), "Join A Lobby"))
+                if (GUI.Button(new Rect(Screen.width / 2 - 300, Screen.height / 2 - 100, 600, 150), "Join a Game"))
                 {
-                    PhotonNetwork.ConnectUsingSettings("V0");
+                    PhotonNetwork.ConnectUsingSettings();
                     state = 4;
                 }
                 break;
 
             //connected to server
             case 1:
-                if (GUI.Button(new Rect(10, 10, 200, 200), "1v1"))
+                if (GUI.Button(new Rect(Screen.width / 2 - 300, Screen.height / 2 - 100, 600, 150), "1v1"))
                 {
-                    PhotonNetwork.JoinLobby();
                     state = 4;
                 }
                 
@@ -46,7 +71,7 @@ public class networkManager : Photon.MonoBehaviour
 
             //connected to lobby
             case 2:
-                if (GUI.Button(new Rect(10, 10, 200, 200), "Find match"))
+                if (GUI.Button(new Rect(Screen.width / 2 - 300, Screen.height / 2 - 100, 600, 150), "Find Match"))
                 {
                     PhotonNetwork.JoinRandomRoom();
                     state = 5;
@@ -54,7 +79,9 @@ public class networkManager : Photon.MonoBehaviour
                 break;
             //connected to game
             case 3:
-
+                mainGame.SetActive(true);
+                GameObject player = PhotonNetwork.Instantiate(mrHan.name, spawnPosition.position, Quaternion.identity);
+                player.GetComponent<playerSetup>().isLocalPlayer();
                 break;
 
             //break
@@ -64,9 +91,11 @@ public class networkManager : Photon.MonoBehaviour
 
             //matchmaking
             case 5:
-                GUI.Label(new Rect(10, Screen.height - 50, 200, 30), "Players: " + PhotonNetwork.playerList.Length.ToString() + "/" + maxPlayers);
+                GUI.Label(new Rect(Screen.width / 2 - 130, Screen.height / 2 - 80, 800, 100), "Queue: " + seconds, styleLab);
 
-                if(PhotonNetwork.inRoom == true && PhotonNetwork.playerList.Length == maxPlayers)
+                GUI.Label(new Rect(Screen.width / 2 - 300, Screen.height / 2 , 800, 100), "Waiting for players (" + PhotonNetwork.PlayerList.Length.ToString() + "/" + maxPlayers + ")...", styleLab);
+
+                if(PhotonNetwork.InRoom == true && PhotonNetwork.PlayerList.Length == maxPlayers)
                 {
                     StartGame();
                     state = 3;
@@ -75,20 +104,20 @@ public class networkManager : Photon.MonoBehaviour
         }
     }
 
-    void OnConnectedToPhoton()
+    public override void OnConnectedToMaster()
     {
+        base.OnConnectedToMaster();
         state = 1;
+        PhotonNetwork.JoinLobby();
     }
 
-    void OnJoinedLobby()
+    public override void OnJoinedLobby()
     {
+        base.OnJoinedLobby();
+        PhotonNetwork.JoinOrCreateRoom("test", null, null);
         state = 2;
     }
 
-    void OnPhotonRandomJoinedFailed()
-    {
-        PhotonNetwork.CreateRoom(null);
-    }
 
     void StartGame()
     {
